f3f6eb938eb22a70ca4f5b1339b941dcbf4cdbef
DS0708 <tlsehdtjd070@naver.com>
2023-12-30 17:03:29 +0900

    Finish

--- js/main.js
+++ js/main.js
@@ -25,25 +25,42 @@ searchInputEl.addEventListener('blur', function () {
  */
 // 페이지 스크롤에 영향을 받는 요소들을 검색!
 const badgeEl = document.querySelector('header .badges')
-
-window.addEventListener('scroll', _.throttle(function() {
-  // console.log(window.scrollY)
-  if(window.scrollY > 500){
-    //배지 숨기기
-    // gsap.to(요소, 지속시간, 옵션);
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
     })
-  }else{
-    //배지 보이기
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
 }, 300))
-// _.throttle( function , 시간 : ms )
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
 
 
 
@@ -167,4 +184,10 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
+
+/**
+ * 올해가 몇 년도인지 계산
+ */
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
 
